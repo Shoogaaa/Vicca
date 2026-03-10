159516e553dd0552ea7380b7f8abd4641cf8a896
Xus! <ericantoniayo@gmail.com>
2026-03-10 12:39:21 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Would you be mine ?'.split('')
+const text = 'Would you be mine?'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
