d56556ec7073dfe8676d812618ed898384ffeb9f
Xus! <ericantoniayo@gmail.com>
2026-03-10 12:35:55 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Will you be my valentine ?'.split('')
+const text = 'Would you be mine ?'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
